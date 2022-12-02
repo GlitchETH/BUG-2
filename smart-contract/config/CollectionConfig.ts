93a81ed4bc284f12726732bf95987b790156f773
GlitchETH <103063172+GlitchETH@users.noreply.github.com>
2022-12-02 17:46:26 -0500

    new

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -16,7 +16,7 @@ const CollectionConfig: CollectionConfigInterface = {
   maxSupply: 1000,
   whitelistSale: {
     price: 0.00,
-    maxMintAmountPerTx: 45,
+    maxMintAmountPerTx: 90,
   },
   preSale: {
     price: 0.00,
