0b50cdefb0071a457879319dc0a71918b650a715
GlitchETH <103063172+GlitchETH@users.noreply.github.com>
2022-12-03 13:16:01 -0500

    aa

--- minting-dapp/src/scripts/react/CollectionStatus.tsx
+++ minting-dapp/src/scripts/react/CollectionStatus.tsx
@@ -46,7 +46,7 @@ export default class CollectionStatus extends React.Component<Props, State> {
             <span className="label">Sale status</span>
             {this.isSaleOpen() ?
               <>
-                {this.props.isWhitelistMintEnabled ? 'Moby Dick' : 'Open'}
+                {this.props.isWhitelistMintEnabled ? 'Glitch City Residence Club' : 'Open'}
               </>
               :
               'Closed'
